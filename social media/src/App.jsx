9c913e5fe15b724f42659e98b221c17bb5165896
ashishmzn11 <ashishmzn11@gmail.com>
2024-10-10 20:36:17 +0530

    second commit

--- social media/src/App.jsx
+++ social media/src/App.jsx
@@ -4,13 +4,19 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import Header from "./component/header"
 import Footer from "./component/footer"
 import Sidebar from "./component/sidebar"
+import PostItem from './component/PostItem'
+import { useState } from 'react'
+import CreatPost from './creatPost'
 function App() {
-
+const [SelectedTab,setSelectedTab]=useState("asgusd")
   return (
 <div className="sidenav">
 <Sidebar/>
 <div className="content">
 <Header/>
+{/* <PostItem/>
+<CreatPost/> */}
+{SelectedTab==="Home"?<PostItem/>:<CreatPost/>}
 <Footer/>
 </div>
 </div>
--- social media/src/component/sidebar.jsx
+++ social media/src/component/sidebar.jsx
@@ -1,7 +1,8 @@
+import './sidebar.css'
 function Sidebar(){
     return(
    
-   <div class="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary" style={{width: "280px"}}>
+   <div class="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary sidebar" style={{width: "280px"}}>
     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
       <svg class="bi pe-none me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
       <span class="fs-4">Sidebar</span>
@@ -18,24 +19,6 @@ function Sidebar(){
         <a href="#" class="nav-link link-body-emphasis">
           <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
           Dashboard
-        </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link link-body-emphasis">
-          <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#table"></use></svg>
-          Orders
-        </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link link-body-emphasis">
-          <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#grid"></use></svg>
-          Products
-        </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link link-body-emphasis">
-          <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#people-circle"></use></svg>
-          Customers
         </a>
       </li>
     </ul>
